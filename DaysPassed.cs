cfcc2a6fef055067b92df526cdaed044261cf18e
AliceD <95102992+4liceD@users.noreply.github.com>
2022-11-14 14:47:56 +0100

    Days passed sent to tmpro
    Set the start date in unix time and get the number of days passed in a tmpro text field

---
+++ DaysPassed.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using TMPro;
+using System;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DaysPassed : UdonSharpBehaviour
+{
+    [SerializeField] int _start;
+    TextMeshPro mText;
+    private double epochStart;
+    private double timestamp;
+    private double difference;
+    private double days;
+    void Start()
+    {
+        var epochStart = new System.DateTime(1970, 1, 1, 8, 0, 0, System.DateTimeKind.Utc);
+        var timestamp = (System.DateTime.UtcNow - epochStart).TotalSeconds;
+        var difference = (timestamp - _start);
+        var days = (difference / 86400);
+        TextMeshPro mText = GetComponent<TextMeshPro>();
+        mText.text = "Day " + Math.Round(days);
+
+    }
+}
