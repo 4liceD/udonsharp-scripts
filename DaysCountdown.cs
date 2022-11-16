24616a94c4c821f9aae6f460793d7d6903a95955
AliceD <95102992+4liceD@users.noreply.github.com>
2022-11-16 15:13:53 +0100

    Countdown in Days
    Counts down to a set date in unix time

---
+++ DaysCountdown.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using TMPro;
+using System;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DaysCountdown : UdonSharpBehaviour
+{
+    [SerializeField] int _stop;
+    TextMeshPro mText;
+    private double epochStart;
+    private double timestamp;
+    private double difference;
+    private double days;
+    void Start()
+    {
+        var epochStart = new System.DateTime(1970, 1, 1, 8, 0, 0, System.DateTimeKind.Utc);
+        var timestamp = (System.DateTime.UtcNow - epochStart).TotalSeconds;
+        var difference = (_stop - timestamp);
+        var days = (difference / 86400);
+        TextMeshPro mText = GetComponent<TextMeshPro>();
+        mText.text = Math.Round(days) + " Days left";
+
+    }
+}
