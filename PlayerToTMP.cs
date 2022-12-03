293b62e885cd45c6d8712e1ffcf7bf7c584ab3b0
AliceD <95102992+4liceD@users.noreply.github.com>
2022-12-03 15:27:37 +0100

    Paste local player name into a textmeshpro textfield

---
+++ PlayerToTMP.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using TMPro;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PlayerToTMP : UdonSharpBehaviour
+{
+    TextMeshPro mText;
+    string playerName;
+    void Start()
+    {
+        var playerName = Networking.LocalPlayer.displayName;
+        TextMeshPro mText = GetComponent<TextMeshPro>();
+        mText.text = playerName;
+    }
+}
