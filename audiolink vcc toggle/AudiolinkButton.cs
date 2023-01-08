9a8d78efeab858a51045a705deed38dccd70fad5
AliceD <95102992+4liceD@users.noreply.github.com>
2023-01-08 17:22:11 +0100

    audiolink vcc toggle

---
+++ audiolink vcc toggle/AudiolinkButton.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+using VRCAudioLink;
+using static VRC.SDKBase.VRCShader;
+
+public class AudiolinkButton : UdonSharpBehaviour
+{
+    [SerializeField] AudioLink udonAudioLink;
+    [SerializeField] Image audiolinkButton;
+    [SerializeField] RenderTexture audiolinkTexture;
+    [SerializeField] Texture2D fakeAudiolinkTexture;
+    bool audiolinkToggle = true;
+
+    public void _AudiolinkToggle()
+    {
+        audiolinkToggle = !audiolinkToggle;
+        if (audiolinkToggle)
+        {
+            SetGlobalTexture(PropertyToID("_AudioTexture"), audiolinkTexture);
+            audiolinkButton.color = Color.green;
+        }
+        else
+        {
+            SetGlobalTexture(PropertyToID("_AudioTexture"), fakeAudiolinkTexture);
+            audiolinkButton.color = Color.red;
+        }
+    }
+}
