9e8c3dcf01a76a12d5976c1f67e7a75d11a466ac
AliceD <95102992+4liceD@users.noreply.github.com>
2023-07-15 14:45:07 +0200

    ltcgi emitter intensity synced and unsynced

---
+++ LTCGI Tools/Scripts/Intensity.cs
@@ -0,0 +1,34 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class Intensity : UdonSharpBehaviour
+{
+    public LTCGI_UdonAdapter ltcgi;
+    public GameObject ltcgiEmitter;
+    int screenIndex;
+    float intensityValue = 1.0f;
+    MaterialPropertyBlock propertyBlock;
+    Renderer EmissionRenderer;
+    Color screenColor;
+    Color screenUpdate;
+    public Slider intensitySlider;
+    void Start()
+    {
+        screenIndex = ltcgi._GetIndex(ltcgiEmitter);
+        EmissionRenderer = ltcgiEmitter.GetComponent<MeshRenderer>();
+        propertyBlock = new MaterialPropertyBlock();
+        screenColor = ltcgi._GetColor(screenIndex);
+    }
+    public void _OnValueChange()
+    {
+        screenUpdate = screenColor * intensitySlider.value;
+        ltcgi._SetColor(screenIndex, screenUpdate);
+        EmissionRenderer.GetPropertyBlock(propertyBlock);
+        propertyBlock.SetColor("_EmissionColor", screenUpdate.gamma);
+        EmissionRenderer.SetPropertyBlock(propertyBlock);
+    }
+}
---
+++ LTCGI Tools/Scripts/intensitySynced.cs
@@ -0,0 +1,46 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class intensitySynced : UdonSharpBehaviour
+{
+    public LTCGI_UdonAdapter ltcgi;
+    public GameObject ltcgiEmitter;
+    int screenIndex;
+    float intensityValue = 1.0f;
+    MaterialPropertyBlock propertyBlock;
+    Renderer EmissionRenderer;
+    Color screenColor;
+    Color screenUpdate;
+    public Slider intensitySlider;
+    [UdonSynced] float syncedValue = 1.0f;
+    void Start()
+    {
+        screenIndex = ltcgi._GetIndex(ltcgiEmitter);
+        EmissionRenderer = ltcgiEmitter.GetComponent<MeshRenderer>();
+        propertyBlock = new MaterialPropertyBlock();
+        screenColor = ltcgi._GetColor(screenIndex);
+    }
+    public void OnValueChange()
+    {
+        if (!Networking.IsOwner(Networking.LocalPlayer, gameObject)) Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        syncedValue = intensitySlider.value;
+        screenUpdate = screenColor * intensitySlider.value;
+        ltcgi._SetColor(screenIndex, screenUpdate);
+        EmissionRenderer.GetPropertyBlock(propertyBlock);
+        propertyBlock.SetColor("_EmissionColor", screenUpdate.gamma);
+        EmissionRenderer.SetPropertyBlock(propertyBlock);
+    }
+    public override void OnDeserialization()
+    {
+        intensitySlider.SetValueWithoutNotify(syncedValue);
+        screenUpdate = screenColor * intensitySlider.value;
+        ltcgi._SetColor(screenIndex, screenUpdate);
+        EmissionRenderer.GetPropertyBlock(propertyBlock);
+        propertyBlock.SetColor("_EmissionColor", screenUpdate.gamma);
+        EmissionRenderer.SetPropertyBlock(propertyBlock);
+    }
+}
